8f09c38fb0f407e445ee8c9c2e5feb4da19ea2c6
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-05 16:53:20 -0700

    services, hooks

---
+++ src/components/Reviews/EditReviews.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function EditReviews() {
+  return (
+    <div>EditReviews</div>
+  );
+}
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useReview from '../../hooks/useReview';
 
 export default function Reviews() {
 
-  const { review, setReview } = useReview();
-  // console.log(review);
+  const { review, loading, error } = useReview();
+  if (loading) return <h2>Loading Review..</h2>;
+  if (error) return <h2>{error}</h2>;
 
   return (
-    <div>Reviews</div>
+    <div>
+      {review.map((review) => {
+        return (
+          <div className="big-div" key={review.id}>
+            
+            <div>{review.restaurant}</div>
+            <div>{review.description}</div>
+            <div> {review.rating} </div>
+            <Link to={`/reviews/editreviews/`}>Edit Review</Link>
+          </div>
+        );
+      })}
+    </div>
   );
 }
