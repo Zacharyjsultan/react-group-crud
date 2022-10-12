b48083617a87333d12607d083aa615fcfb0b85cf
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-12 13:12:47 -0700

    test 1

--- src/App.test.js
+++ src/App.test.js
@@ -3,19 +3,21 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './context/UserContext';
 
+// importing services
 import * as authFns from './services/auth';
-import * as ReviewsFns from './services/posts';
 
+// allows testing to occur
 jest.mock('./services/auth');
+jest.mock('./services/reviews');
 
 const mockUser = {
   id: '0dab2c65-5911-469c-9f12-8fb47ebe52f2',
   aud: 'authenticated',
   role: 'authenticated',
-  email: 'example@example.com',
+  email: 'random@example.com',
 };
 
-test('auth log in ', () => {
+test('auth log in ', async () => {
   authFns.getUser.mockReturnValue(null);
   authFns.authUser.mockReturnValue(mockUser);
   render(
@@ -25,6 +27,13 @@ test('auth log in ', () => {
       </MemoryRouter>
     </UserProvider>
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailInput = screen.getByLabelText('Email=');
+  fireEvent.change(emailInput, { target: { value: 'random@example.com' } });
+  expect(emailInput.value).toBe('random@example.com');
+
+  const passwordInput = screen.getByLabelText('Password=');
+  fireEvent.change(passwordInput, { target: { value: '123456' } });
+
+  const button = screen.getByRole('button');
+  fireEvent.click(button);
 });
