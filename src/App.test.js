21bba5091ef6592a2d2421f1cb64259d97bd864a
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-12 11:01:00 -0700

    attempt

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { UserProvider } from './context/UserContext';
 
-test('renders learn react link', () => {
-  render(<App />);
+import * as authFns from './services/auth';
+import * as ReviewsFns from './services/posts';
+
+jest.mock('./services/auth');
+
+const mockUser = {
+  id: '0dab2c65-5911-469c-9f12-8fb47ebe52f2',
+  aud: 'authenticated',
+  role: 'authenticated',
+  email: 'zach@example.com',
+};
+
+test('auth log in ', () => {
+  authFns.getUser.mockReturnValue(null);
+  authFns.authUser.mockReturnValue(mockUser);
+  render(
+    <UserProvider>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </UserProvider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
